14614f1638b2b26b6934b6556758e1e32e92552d
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-18 20:23:11 +0300

    Complete javascript exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  arr = arr.filter((e) => !args.includes(e));
+  return arr;
 };
 
 // Do not edit below this line
