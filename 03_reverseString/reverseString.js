6b4a69fa7f79e9119451ba0a63d48153ea4d7d4f
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-18 19:22:24 +0300

    Complete javascript exercises 02 and 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (inputString) {
+  reverse = "";
+  for (i = inputString.length - 1; i >= 0; i--) {
+    reverse += inputString.charAt(i);
+  }
+  return reverse;
 };
 
 // Do not edit below this line
