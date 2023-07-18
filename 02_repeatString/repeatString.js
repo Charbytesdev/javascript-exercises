fc51702f76430f9df2d49a6c7ce2b7aa6348cfd8
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-18 19:22:24 +0300

    Complete javascript exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, repeats) {
+  if (repeats < 0) return "ERROR";
+  let outputString = "";
+  for (let i = 0; i < repeats; i++) {
+    outputString += word;
+  }
+  return outputString;
 };
 
 // Do not edit below this line
