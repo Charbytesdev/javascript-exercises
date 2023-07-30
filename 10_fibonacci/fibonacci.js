f73f6957840039ddfd693db5da2c6610661ffe1a
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-30 15:33:41 +0300

    Complete javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  let prev = 1;
+  let prevprev = 0;
+  let output = 1;
+  for (let i = 1; i < num; i++) {
+    output = prev + prevprev;
+    prevprev = prev;
+    prev = output;
+  }
+  return output;
 };
 
 // Do not edit below this line
