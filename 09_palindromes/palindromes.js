691d3add5aa3d0a7c800bbf8f772686ee1644ec7
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-30 15:18:04 +0300

    Complete javascript exersise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  let normal = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversed = normal.split("").reverse().join("");
+  return normal === reversed;
 };
 
 // Do not edit below this line
