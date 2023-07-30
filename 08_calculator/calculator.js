d66d905891d08d4d14695eb506c6d1f988850878
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-30 14:57:42 +0300

    Finish javascript exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, current) => acc + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (x, y, ...others) {
+  return x * y * others.reduce((acc, current) => acc * current, 1);
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  let negativePower = false;
+  let sum = number;
+  if (power === 0) return 1;
+  if (power < 0) {
+    negativePower = true;
+    power *= -1;
+  }
+  for (let index = 1; index < power; index++) {
+    sum *= number;
+  }
+  if (negativePower) return 1 / sum;
+  else return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let sum = 1;
+  for (let i = 1; i < n + 1; i++) {
+    sum *= i;
+  }
+  return sum;
+};
+
+const recursiveFactorial = function (n) {
+  if (n === 0) return 1;
+  return n * recursiveFactorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
